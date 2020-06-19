8e0af83b765330815dd821aacc11ac0b6aa21854
Michael Nowotny <nowotnym@gmail.com>
2020-06-18 21:44:52 -0700

    - removed timing statements from jupyter notebook

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ URL = 'https://github.com/michaelnowotny/divergence'
 EMAIL = 'nowotnym@gmail.com'
 AUTHOR = 'Michael Christoph Nowotny'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = "0.3.2"
+VERSION = "0.3.3"
 
 # What packages are required for this module to be executed?
 REQUIRED = [
