38e79cac2b42ab23c3359ba0d65a0b1297ef32e9
Michael Nowotny <nowotnym@gmail.com>
2020-06-30 20:35:09 -0700

    - added joint entropy and conditional entropy to example notebook

--- divergence/continuous.py
+++ divergence/continuous.py
@@ -1034,3 +1034,148 @@ def joint_entropy_from_samples(sample_x: np.ndarray,
                                   log_fun=log_fun,
                                   eps_abs=eps_abs,
                                   eps_rel=eps_rel)
+
+
+################################################################################
+# Conditional Entropy
+###############################################################################
+def conditional_entropy_from_densities_with_support(pdf_x: tp.Callable,
+                                                    pdf_xy: tp.Callable,
+                                                    x_min: float,
+                                                    x_max: float,
+                                                    y_min: float,
+                                                    y_max: float,
+                                                    log_fun: tp.Callable = np.log,
+                                                    eps_abs: float = 1.49e-08,
+                                                    eps_rel: float = 1.49e-08
+                                                    ) -> float:
+    """
+    Compute conditional entropy of the random variables x and y with joint density p_{x, y} and
+    marginal density p_x defined as
+
+            H(Y|X) = - E_{p_{x, y}} \left[ \log \frac{p_{x, y} (x, y)}{p_x(x)} \right]
+
+    via numerical integration on a rectangular domain aligned with the axes.
+    The argument log_fun can be used to specify the units in which the entropy is measured.
+    The default choice is the natural logarithm.
+
+    Parameters
+    ----------
+    pdf_x: probability density function of the random variable x
+    pdf_xy: joint probability density function of the random variables x and y
+    x_min: lower bound of the integration domain for x
+    x_max: upper bound of the integration domain for x
+    y_min: lower bound of the integration domain for y
+    y_max: upper bound of the integration domain for y
+    log_fun: logarithmic function to control the units of measurement for the result
+    eps_abs: absolute error tolerance for numerical integration
+    eps_rel: relative error tolerance for numerical integration
+
+    Returns
+    -------
+    The conditional entropy of the random variables x and y
+    """
+    def conditional_entropy_integrand(x: float, y: float):
+        pxy = pdf_xy((x, y))
+        px = pdf_x(x)
+
+        return pxy * log_fun(pxy / px)
+
+    return - sp.integrate.dblquad(conditional_entropy_integrand,
+                                  a=x_min,
+                                  b=x_max,
+                                  gfun=lambda x: y_min,
+                                  hfun=lambda x: y_max,
+                                  epsabs=eps_abs,
+                                  epsrel=eps_rel)[0]
+
+
+def conditional_entropy_from_kde(kde_x: sm.nonparametric.KDEUnivariate,
+                                 kde_xy: sp.stats.kde.gaussian_kde,
+                                 y_min: float,
+                                 y_max: float,
+                                 log_fun: tp.Callable = np.log,
+                                 eps_abs: float = 1.49e-08,
+                                 eps_rel: float = 1.49e-08) -> float:
+    """
+    Compute conditional entropy of the random variables x and y with joint density p_{x, y} and
+    marginal density p_x defined as
+
+            H(Y|X) = - E_{p_{x, y}} \left[ \log \frac{p_{x, y} (x, y)}{p_x(x)} \right]
+
+    via numerical integration, where the marginal density of x is given by a statsmodels kde object
+    and the joint density by a SciPy gaussian_kde object.
+    The argument log_fun can be used to specify the units in which the entropy is measured.
+    The default choice is the natural logarithm.
+
+    Parameters
+    ----------
+    kde_x: statsmodels kde object approximating the marginal density of x
+    kde_y: statsmodels kde object approximating the marginal density of y
+    kde_xy: SciPy gaussian_kde object approximating the joint density of x and y
+    y_min: lower bound of the integration domain for y
+    y_max: upper bound of the integration domain for y
+    log_fun: logarithmic function to control the units of measurement for the result
+    eps_abs: absolute error tolerance for numerical integration
+    eps_rel: relative error tolerance for numerical integration
+
+    Returns
+    -------
+    The conditional entropy of the random variables x and y
+    """
+    x_min = min(kde_x.support)
+    x_max = max(kde_x.support)
+
+    return conditional_entropy_from_densities_with_support(pdf_x=kde_x.evaluate,
+                                                           pdf_xy=kde_xy.pdf,
+                                                           x_min=x_min,
+                                                           x_max=x_max,
+                                                           y_min=y_min,
+                                                           y_max=y_max,
+                                                           log_fun=log_fun,
+                                                           eps_abs=eps_abs,
+                                                           eps_rel=eps_rel)
+
+
+def conditional_entropy_from_samples(sample_x: np.ndarray,
+                                     sample_y: np.ndarray,
+                                     log_fun: tp.Callable = np.log,
+                                     eps_abs: float = 1.49e-08,
+                                     eps_rel: float = 1.49e-08) -> float:
+    """
+    Compute conditional entropy of the random variables x and y with joint density p_{x, y} and
+    marginal density p_x defined as
+
+            H(Y|X) = - E_{p_{x, y}} \left[ \log \frac{p_{x, y} (x, y)}{p_x(x)} \right]
+
+    from samples of the two distributions via approximation by kernel density estimates and
+    numerical integration.
+    The argument log_fun can be used to specify the units in which the entropy is measured.
+    The default choice is the natural logarithm.
+
+    Parameters
+    ----------
+    sample_x: x-component of the sample from the joint density p_{x, y}
+    sample_y: y-component of the sample from the joint density p_{x, y}
+    log_fun: logarithmic function to control the units of measurement for the result
+    eps_abs: absolute error tolerance for numerical integration
+    eps_rel: relative error tolerance for numerical integration
+
+    Returns
+    -------
+    The conditional entropy of the random variables x and y
+    """
+    kde_x = sm.nonparametric.KDEUnivariate(sample_x)
+    kde_x.fit()
+    kde_y = sm.nonparametric.KDEUnivariate(sample_y)
+    kde_y.fit()
+
+    kde_xy = sp.stats.gaussian_kde([sample_x, sample_y])
+
+    return conditional_entropy_from_kde(kde_x=kde_x,
+                                        kde_xy=kde_xy,
+                                        y_min=min(kde_y.support),
+                                        y_max=max(kde_y.support),
+                                        log_fun=log_fun,
+                                        eps_abs=eps_abs,
+                                        eps_rel=eps_rel)
